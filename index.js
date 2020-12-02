c3f8cee9f96442738982fa658269c330f6ecb27f
Carolina Urrea <carolinaurrea@Carolinas-MacBook-Pro.local>
2020-12-01 19:30:49 -0500

    implement cheerio and am able to get objects back but can't single the one object I want

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ const express = require("express");
 const app = express();
 const cheerio = require('cheerio');
 const request = require('request');
+const puppeteer = require('puppeteer');
+
+// async function scrapeProduct(url) {
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.goto(url);
+
+//     const [el] = await page.$x('/html/body/div[2]/table/tbody/tr[11]/td[2]/span');
+    
+//     const text = await el.getProperties('textContent');
+    
+//     const rawText = await text.jsonValue();
+    
+//     console.log({srcText});
+//     browser.close();
+// }
+// scrapeProduct('https://www.numbeo.com/cost-of-living/in/Washington');
 
 app.set('view engine', 'ejs');
 
@@ -15,28 +32,30 @@ app.get('/', (req, res) => {
     res.render('index', { alerts: res.locals.alerts });
   });
 
-  request('https://www.numbeo.com/cost-of-living/in/Washington', (error, response, body) => {
-    let $ = cheerio.load(body);
-    let results = $('.search-result-preview')
-    let resultTitles = results.map((index, element)=>{
-        return $(element).find('td').attr('Meal, Inexpensive Restaurant')
-    })
-    console.log(resultTitles)
-})
-
-// request({
-//     method: 'GET',
-//     url: 'https://www.numbeo.com/cost-of-living/'
-// }, (err, res, body) => {
-
-//     if (err) return console.error(err);
-
+//   request('https://www.numbeo.com/cost-of-living/in/Washington', (error, response, body) => {
 //     let $ = cheerio.load(body);
+//     let results = $('.search-result-preview')
+//     let resultTitles = results.map((index, element)=>{
+//         return $(element).find('<td>Meal, Inexpensive Restaurant </td>').attr('meal')
+        
+//     })
+//     console.log(resultTitles)
+// })
 
-//     let title = $('title');
+request({
+    method: 'GET',
+    url: 'https://www.numbeo.com/cost-of-living/in/Los-Angeles'
+}, (err, res, body) => {
 
-//     console.log(title.text());
-// });
+    if (err) return console.error(err);
+
+    let $ = cheerio.load(body);
+    
+
+    let title = $('tr').attr('class', 'first_currency');
+    console.log(title.text());
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/thing", (req, res) => res.send("Connected"));
