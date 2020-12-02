d24b3d216575fa7cbbcb068a267477f976e8a685
Carolina Urrea <carolinaurrea@Carolinas-MacBook-Pro.local>
2020-12-01 23:41:33 -0500

    added the routes and I got an object to show

--- index.js
+++ index.js
@@ -3,21 +3,12 @@ const app = express();
 const cheerio = require('cheerio');
 const request = require('request');
 const puppeteer = require('puppeteer');
+const layouts = require('express-ejs-layouts');
+const session = require('express-session');
+const passport = require('./config/ppConfig');
+const flash = require('connect-flash');
+const SECRET_SESSION = process.env.SECRET_SESSION;
 
-// async function scrapeProduct(url) {
-//     const browser = await puppeteer.launch();
-//     const page = await browser.newPage();
-//     await page.goto(url);
-
-//     const [el] = await page.$x('/html/body/div[2]/table/tbody/tr[11]/td[2]/span');
-    
-//     const text = await el.getProperties('textContent');
-    
-//     const rawText = await text.jsonValue();
-    
-//     console.log({srcText});
-//     browser.close();
-// }
 // scrapeProduct('https://www.numbeo.com/cost-of-living/in/Washington');
 
 app.set('view engine', 'ejs');
@@ -26,12 +17,46 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 //app.use(layouts);
+const sessionObject = {
+    secret: SECRET_SESSION,
+    resave: false,
+    saveUninitialized: true
+  }
+  
+  app.use(session(sessionObject));
+  
+  // Initialize passport and run through middleware
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
+  // Flash
+  // Using flash throughout app to send temp messages to user
+  app.use(flash());
+  
+  // Messages that will be accessible to every view
+  app.use((req, res, next) => {
+    // Before every route, we will attach a user to res.local
+    res.locals.alerts = req.flash();
+    res.locals.currentUser = req.user;
+    next();
+  });
 
 app.get('/', (req, res) => {
     console.log(res.locals.alerts);
     res.render('index', { alerts: res.locals.alerts });
   });
-
+//   const website = {
+//     method: 'GET',
+//     url: 'https://www.numbeo.com/cost-of-living/in/Washington'
+//   };
+//   request(website, (err, res, body) => {
+//       if (err) return console.error(err);
+//       let $ = cheerio.load(body);
+//       let title = $('tr').attr('class', 'first_currency');
+//       // console.log(title.text());
+//       let milk = $('.first_currency').find('span').text().split('\n')[10];
+//       console.log(milk);
+//   });
 //   request('https://www.numbeo.com/cost-of-living/in/Washington', (error, response, body) => {
 //     let $ = cheerio.load(body);
 //     let results = $('.search-result-preview')
@@ -42,26 +67,33 @@ app.get('/', (req, res) => {
 //     console.log(resultTitles)
 // })
 
-request({
+const website = {
     method: 'GET',
     url: 'https://www.numbeo.com/cost-of-living/in/Los-Angeles'
-}, (err, res, body) => {
+  };
+  request(website, (err, res, body) => {
+      if (err) return console.error(err);
+      let $ = cheerio.load(body);
+      let title = $('tr').attr('class', 'first_currency');
+      // console.log(title.text());
+      let milk = $('.first_currency').find('span').text().split('\n')[10];
+      console.log(milk);
+  });
 
-    if (err) return console.error(err);
+const PORT = process.env.PORT || 8000;
 
-    let $ = cheerio.load(body);
-    
-
-    let title = $('tr').attr('class', 'first_currency');
-    console.log(title.text());
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.get("/thing", (req, res) => res.send("Connected"));
+app.get("/thing", (req, res) => res.send("Connected")); 
 
 const server = app.listen(PORT, () => {
   console.log(`You are connected ${PORT} 🎧`);
 });
 
 module.exports = server;
+
+
+
+// const list = $('table[class="data_wide_table new_bar_table"]')
+// .find('tbody > tr > td > span[class="first_currenecy"]')
+// .toArray()
+// .map(element => $(element).textContent);
+// console.log(list);
