0d28f56775eabefc45508a91a15ca30979929715
Carolina Urrea <carolinaurrea@Carolinas-MacBook-Pro.local>
2020-12-04 16:25:50 -0500

    [README.md] added the steps to create this repo, working on connecting my files

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# dev_jobs_usa
-Job search for Web Developers in the US by state and city 
+### dev_jobs_usa
+Job search for Web Developers in the US by state and city.
+Hopes are to make an informed decision of what ever city you might be moving to.
+
+Express authentication template using Cheerio + Passport + flash messages + custom middleware
+
+## What it includes
+
+* Sequelize user model / migration
+* Settings for PostgreSQL
+* Passport and passport-local for authentication
+* Sessions to keep user logged in between pages
+* Flash messages for errors and successes
+* Passwords that are hashed with BCrypt
+* EJS Templating and EJS Layouts
+
+## What it includes
+
+* Sequelize user model / migration
+* Settings for PostgreSQL
+* Passport and passport-local for authentication
+* Sessions to keep user logged in between pages
+* Flash messages for errors and successes
+* Passwords that are hashed with BCrypt
+* EJS Templating and EJS Layouts
+
+### User Model
+
+| Column Name | Data Type | Notes |
+| --------------- | ------------- | ------------------------------ |
+| id | Integer | Serial Primary Key, Auto-generated |
+| name | String | Must be provided |
+| email | String | Must be unique / used for login |
+| password | String | Stored as a hash |
+| createdAt | Date | Auto-generated |
+| updatedAt | Date | Auto-generated |
+
+
+
+## Steps To Use
+#### 1. Fork and Clone repo 'dev_jobs_usa'
+* Go to `https://github.com/canourrea23/dev_jobs_usa` and fork this repository or go to `https://jobs-for-developers.herokuapp.com/` to check it out. 
+* Clone your new repo to your local machine.
+
+#### 2. You will need to add all the dependencies
+```
+npm i   
+```
+#### 3. Create a new database for the new project
+Using the sequelize command line interface, you can create a new database from the terminal.
+
+```
+createdb <new_db_name>
+```
+
+#### 4. Run the migrations
+
+```
+sequelize db:migrate
+```
+#### 5. Add a `.env` file with the following fields:
+
+* SESSION_SECRET: Can be any random string; usually a hash in production
+* PORT: Usually 3000 or 8000
+
+#### 6. Run server; make sure it works
+
+```
+nodemon
+```
+
--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const app = express();
 require('dotenv').config()
 const cheerio = require('cheerio');
 const request = require('request');
-//const layouts = require('express-ejs-layouts');
+const layouts = require('express-ejs-layouts');
 const session = require('express-session');
 const passport = require('./config/ppConfig');
 const flash = require('connect-flash');
 const db = require("./models");
 const SECRET_SESSION = process.env.SECRET_SESSION;
+const isLoggedIn = require('./middleware/isLoggedIn');
 console.log('SECRET_SESSION', SECRET_SESSION);
 
 app.set('view engine', 'ejs');
@@ -16,7 +17,7 @@ app.set('view engine', 'ejs');
 app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
-//app.use(layouts);
+app.use(layouts);
 const sessionObject = {
     secret: SECRET_SESSION,
     resave: false,
@@ -40,57 +41,78 @@ res.locals.alerts = req.flash();
 res.locals.currentUser = req.user;
 next();
 });
+app.get('/', (req, res) => {
+  console.log(res.locals.alerts);
+  res.render('index', { alerts: res.locals.alerts });
+});
 
-app.get('/profile', (req, res) => {
+app.get('/profile',  (req, res) => {
     //console.log(res.locals.alerts);
     res.render('profile');
   });
+  app.get('/:city', async (req, res) => {
+    const city = req.params.city[0].toUpperCase() + req.params.city.slice(1).toLowerCase();
+    console.log('CITY',city);
+    // const { currency = 'CAD' } = req.query;
+    console.log(req.query);
+    fetch(`https://www.numbeo.com/cost-of-living/in/${city}`, (error, meta, body) => {
+      let html = body.toString();
+      const $ = cheerio.load(html);
 
+      const priceRows = $('body > div.innerWidth > table.data_wide_table.new_bar_table > tbody > tr > td.priceValue > span.first_currency').text().split;
+      
+      console.log(priceRows);
 
-app.get('/city/:city', (req, res, params) => {
-   let city = req.params.city 
-  console.log(city) 
-const requestObject = {
-    method: 'GET',
-    url: `https://www.numbeo.com/cost-of-living/in/${city}`
-  };
-  request(requestObject, (err, res, body) => {
-      if (err) return console.error(err);
-      let $ = cheerio.load(body);
-      let title = $('tr').attr('class', 'first_currency');
-      let milkPrice = Number($('.first_currency').find('span').text().split('\n')[10].split('$')[0]);                                                                    
-      let gasPrice = Number($('.first_currency').find('span').text().split('\n')[35].split('--')[1].slice(5).split('$')[0]);
-      let publicTransit = Number($('.first_currency').find('span').text().split('\n')[31].split('--')[1].slice(4).split('$')[0]);
-      let utilitiesPrice = Number($('.first_currency').find('span').text().split('\n')[39].split('$')[0]);
-      let internetPrice = Number($('.first_currency').find('span').text().split('\n')[41].split('--')[1].slice(4).split('$')[0]);
-      let childCarePrice = $('.first_currency').find('span').text().split('\n')[47].split(' ')[0].split('$')[0];
-      let childCarePriceParsed = Number(childCarePrice.split(',')[0] + childCarePrice.split(',')[1]);
-      const arrayOfData = $('.first_currency').find('span').text().split('\n')
-      let centre = Number(arrayOfData[55].split('$')[0].split(',')[0] + arrayOfData[55].split('$')[0].split(',')[1]);    
-      let outOfCity = Number(arrayOfData[56].split('$')[0].split(',')[2][arrayOfData[56].split('$')[0].split(',')[2].length -1] + arrayOfData[56].split('$')[0].split(',')[3]);   
-      let averageMeal = Number($('.first_currency').find('span').text().split('\n')[1].split('$')[0]);
-        const locationObject = { 
-            bedroom_in_city: Math.round(centre),
-            bedroom_outside_centre: Math.round(outOfCity),
-            public_transit: Math.round(publicTransit),
-            internet: Math.round(internetPrice),
-            childcare:  Math.round(childCarePriceParsed),
-            gas: Math.round(gasPrice),
-            average_meal: Math.round(averageMeal),
-            milk: Math.round(milkPrice),
-            utilities: Math.round(utilitiesPrice),
-        }
-        console.log(locationObject);
-        // console.log(Math.round(locationObject.bedroom_in_city));
-        
-        db.location.create(locationObject)
-        .then((newLocation) => {
-            console.log(newLocation.get());
-
-        })
-  });
-  res.send('city')
+    })
 });
+
+
+// app.get('/city/:city', (req, res, params) => {
+//    let city = req.params.city 
+//   console.log(city) 
+  
+// const requestObject = {
+//     method: 'GET',
+//     url: `https://www.numbeo.com/cost-of-living/in/${city}`
+//   };
+//   request(requestObject, (err, res, body) => {
+//     if (err) return console.error(err);
+//     let $ = cheerio.load(body);
+//     let title = $('tr').attr('class', 'first_currency');
+//     let milkPrice = Number($('.first_currency').find('span').text().split('\n')[10].split('$')[0]); 
+//     console.log(milkPrice);                                                                   
+//     let gasPrice = Number($('.first_currency').find('span').text().split('\n')[35].split('--')[1].slice(5).split('$')[0]);
+//     let publicTransit = Number($('.first_currency').find('span').text().split('\n')[31].split('--')[1].slice(4).split('$')[0]);
+//     let utilitiesPrice = Number($('.first_currency').find('span').text().split('\n')[39].split('$')[0]);
+//     let internetPrice = Number($('.first_currency').find('span').text().split('\n')[41].split('--')[1].slice(4).split('$')[0]);
+//     let childCarePrice = $('.first_currency').find('span').text().split('\n')[47].split(' ')[0].split('$')[0];
+//     let childCarePriceParsed = Number(childCarePrice.split(',')[0] + childCarePrice.split(',')[1]);
+//     const arrayOfData = $('.first_currency').find('span').text().split('\n')
+//     let centre = Number(arrayOfData[55].split('$')[0].split(',')[0] + arrayOfData[55].split('$')[0].split(',')[1]);    
+//     let outOfCity = Number(arrayOfData[56].split('$')[0].split(',')[2][arrayOfData[56].split('$')[0].split(',')[2].length -1] + arrayOfData[56].split('$')[0].split(',')[3]);   
+//     let averageMeal = Number($('.first_currency').find('span').text().split('\n')[1].split('$')[0]);
+//       const locationObject = { 
+//           bedroom_in_city: Math.round(centre),
+//           bedroom_outside_centre: Math.round(outOfCity),
+//           public_transit: Math.round(publicTransit),
+//           internet: Math.round(internetPrice),
+//           childcare:  Math.round(childCarePriceParsed),
+//           gas: Math.round(gasPrice * 3.785),
+//           average_meal: Math.round(averageMeal),
+//           milk: Math.round(milkPrice * 3.785),
+//           utilities: Math.round(utilitiesPrice),
+//       }
+//       console.log(locationObject);
+//       // console.log(Math.round(locationObject.bedroom_in_city));
+      
+//       db.location.create(locationObject)
+//       .then((newLocation) => {
+//         console.log(newLocation.get());
+        
+//         res.render('city', { city, newLocation });
+//       })
+//   });
+// });
 //
 const PORT = process.env.PORT || 3000;
 
